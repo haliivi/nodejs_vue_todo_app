7bd10bab45f1fefe0196a5106f8376d08dd1ef70
a.sotnichenko <haliivi@mail.ru>
2023-08-22 17:03:30 +0700

    Updated task done.

--- routes/todo.js
+++ routes/todo.js
@@ -28,9 +28,12 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     try {
-        
+        const todo = await Todo.findByPk(+req.params.id)
+        todo.done = req.body.done
+        await todo.save()
+        res.status(200).json({todo})
     } catch (e) {
         res.status(500).json({
             message: 'Server error'
