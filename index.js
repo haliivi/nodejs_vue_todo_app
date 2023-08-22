3f9dc056bc60c6fc2ec9d30bd6b881e8b7e211c5
a.sotnichenko <haliivi@mail.ru>
2023-08-22 13:39:14 +0700

    Write todoRoutes and plug in to index.js.

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const app = express()
+const todoRoutes = require('./routes/todo')
 const PORT = process.env.PORT || 3000
 
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/api/todo', todoRoutes)
 
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
